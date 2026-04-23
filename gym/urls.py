d1b5fc91640b979352f49f86855394d13211fe35
Urieel <urielsalazar.dev@gmail.com>
2026-04-23 17:38:42 -0600

    Get post and put

--- gym/urls.py
+++ gym/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from django.http import JsonResponse
 from django.contrib import admin
 from gym import views
-
+from .views import UserView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/',views.home)
+    path('users/',UserView.as_view())
 ]
 
--- gym/views.py
+++ gym/views.py
@@ -3,7 +3,27 @@ from .models import User
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from .serializers import UserSerializer
+from rest_framework.views import APIView
 
 
 def home(request):
     return JsonResponse({"ok": True})
+
+class UserView(APIView):
+    def get(self, request):
+    
+        users = User.objects.all()
+        serializer = UserSerializer(users, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+     
+    def post(self,request,format=None):
+          serializer = UserSerializer(data=request.data)
+          if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            
+    def put(self,request):
+         pass
+    def delete(self,request):
+         pass
