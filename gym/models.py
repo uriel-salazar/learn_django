6688f79bd5813c9cb287bb2d41b05e09f9d65c93
Urieel <urielsalazar.dev@gmail.com>
2026-04-27 13:24:28 -0600

    Create one to many relationship between User and Spots table

--- gym/models.py
+++ gym/models.py
@@ -5,4 +5,14 @@ class User(models.Model):
     name = models.CharField(max_length=30)
     age = models.IntegerField()
     email = models.EmailField(max_length=20,unique=True)
+    created_at = models.DateTimeField(auto_created = True,blank=True)
+
+class Spots(models.Model):
+    name_spot = models.CharField(max_length = 30,null = False)
+    body = models.TextField(max_length = 200, null= True)
+    user = models.ForeignKey(User,on_delete = models.CASCADE)
+    created_at = models.TimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.name_spot
     
