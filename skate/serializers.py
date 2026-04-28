b709a2345953c242afbf908edcdc3aeafcea3900
Urieel <urielsalazar.dev@gmail.com>
2026-04-28 10:56:59 -0600

    Add SpotSerializer and SpotsView for managing spots data

--- skate/serializers.py
+++ skate/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import User
+from .models import User,Spots
 from django.forms import forms
 
 class UserSerializer(serializers.ModelSerializer):
@@ -11,3 +11,11 @@ class UserSerializer(serializers.ModelSerializer):
         if len(value) < 5:
             raise serializers.ValidationError("Minimum 5 characters required.")
         return value
+    
+class SpotSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Spots
+        fields = ['id', 'name_spot', 'body', 'user', 'created_at']
+        read_only_fields=['id','created_at']
+        
+        
