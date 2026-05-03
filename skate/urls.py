2a30b3496491c3b194f9f7800c46ad4e5d7c4e76
Urieel <urielsalazar.dev@gmail.com>
2026-05-03 13:14:11 -0600

    Implement UserView Set  and routers

--- skate/urls.py
+++ skate/urls.py
@@ -1,17 +1,16 @@
-from django.urls import path,include
-from django.http import JsonResponse
+from django.urls import path
 from django.contrib import admin
+from .routers import router
 from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView
-from skate import views
-from .views import UserView,UserDetail,SpotsView,SpotsDetail,home
+from .views import SpotsView,SpotsDetail,home
+
 
 urlpatterns = [
     path('',home), # html template 
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/users/',router.urls),
     path('admin/', admin.site.urls),
-    path('users/',UserView.as_view()),
-    path('users/<int:id>/',UserDetail.as_view()),
     path('spots/',SpotsView.as_view()),
     path('spots/<int:id>/',SpotsDetail.as_view())
 ]
