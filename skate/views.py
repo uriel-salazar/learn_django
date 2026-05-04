6394840fe8aa8e0bab8bb5572800c36de18c4b96
Urieel <urielsalazar.dev@gmail.com>
2026-05-04 16:50:46 -0600

    Rewrite put to use a Spot Serializer and raise 400 error

--- skate/views.py
+++ skate/views.py
@@ -52,11 +52,11 @@ class SpotsView(APIView):
         
     def put(self,request,id):
         spots = get_object_or_404(Spots,pk=id)
-        serializer = UserSerializer(spots,data=request.data)
+        serializer = SpotSerializer(spots,data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
-        return Response(status=status.HTTP_404_NOT_FOUND)
+        return Response(status=status.HTTP_400_BAD_REQUEST)
     
     
     def delete(self, request, id):
@@ -72,7 +72,9 @@ class SpotsView(APIView):
         spot.delete()
         return Response({"message": "Deleted successfully"}, status=status.HTTP_204_NO_CONTENT)
    
-        
+
+class Ranking(APIView):
+    pass
 
      
 
