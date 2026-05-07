dbffff6fe01e94bf28460ea0019954dc115af1a2
Urieel <urielsalazar.dev@gmail.com>
2026-05-07 16:15:19 -0600

    Use modelviewset with spots

--- skate/routers.py
+++ skate/routers.py
@@ -1,8 +1,9 @@
 from rest_framework.routers import DefaultRouter
-from .views import UserViewSet,SpotsView
+from .views import UserViewSet,SpotsViewSet
 router = DefaultRouter()
 
 router.register(r'users', UserViewSet)
+router.register(r'spots',SpotsViewSet)
 
 
 
--- skate/views.py
+++ skate/views.py
@@ -1,11 +1,9 @@
-from django.http import JsonResponse
+
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.parsers import MultiPartParser, FormParser
-from django.shortcuts import get_object_or_404,render
+from django.shortcuts import render
 from .models import User,Spots
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
-from rest_framework import status
 from .serializers import UserSerializer,SpotSerializer
 from rest_framework.views import APIView
 
@@ -19,62 +17,16 @@ class UserViewSet(ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     
- 
-class SpotsView(APIView):
+class SpotsViewSet(ModelViewSet):
     permission_classes = [IsAuthenticated]
+    queryset= Spots.objects.all()
     parser_classes = (MultiPartParser, FormParser)
-
-    def get(self,request,id=None):
-        if id: # get user by id :
-            try:
-                spots = Spots.objects.get(pk=id)
-                data={
-                "name_spot": spots.name_spot,
-                "body": spots.body
-                }
-                return JsonResponse(data)
-            except User.DoesNotExist:
-                return Response(status=status.HTTP_404_NOT_FOUND)
-            
-        # To get a list of users 
-        spots = Spots.objects.all()
-        serializer = SpotSerializer(spots, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+    serializer_class= SpotSerializer
     
-   
-    def post(self,request):
-        
-        serializer = SpotSerializer(data=request.data)
-        if serializer.is_valid():
-
-            serializer.save(user=request.user)
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        else:
-            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
-        
-    def put(self,request,id):
-        spots = get_object_or_404(Spots,pk=id)
-        serializer = SpotSerializer(spots,data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+  #  injects to the actual user before saving it.
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
     
-    
-    def delete(self, request, id):
-        """
-        Deletes a spot, and if the spot isn't founded, it raises a 404 status code
-        with a descriptive message. 
-        """
-        try:
-            spot = Spots.objects.get(pk=id)
-        except Spots.DoesNotExist:
-                return Response({"error": " Not found"}, status=status.HTTP_404_NOT_FOUND)
-
-        spot.delete()
-        return Response({"message": "Deleted successfully"}, status=status.HTTP_204_NO_CONTENT)
-   
-
 class Ranking(APIView):
     pass
 
