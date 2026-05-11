71667865d647f6dda8258dc05005b957a22d6d50
Urieel <urielsalazar.dev@gmail.com>
2026-05-11 13:15:05 -0600

    Add LimitOffsetPagination to all my endpoints

--- skate/views.py
+++ skate/views.py
@@ -6,7 +6,7 @@ from .models import User,Spots,Rating
 from rest_framework.permissions import IsAuthenticated
 from .serializers import UserSerializer,SpotSerializer,RatingSerializer
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from .pagination import CostumPagination
+from rest_framework.pagination import LimitOffsetPagination
 
 
 def home(request):
@@ -15,12 +15,14 @@ def home(request):
 
 class UserViewSet(ModelViewSet):
     permission_classes = [IsAuthenticated]
+    pagination_class = LimitOffsetPagination
     queryset = User.objects.all()
     serializer_class = UserSerializer
     
 class SpotsViewSet(ModelViewSet):
     permission_classes = [IsAuthenticated]
     queryset= Spots.objects.all()
+    pagination_class = LimitOffsetPagination
     parser_classes = (MultiPartParser, FormParser)
     serializer_class= SpotSerializer
     
@@ -33,6 +35,7 @@ class RatingViewSet(ModelViewSet):
     serializer_class = RatingSerializer
     mini_serializer_class= RatingSerializer
     queryset = Rating.objects.all()
+    pagination_class = LimitOffsetPagination
     permission_classes = [IsAuthenticated]
     
     def perform_create(self, serializer):
